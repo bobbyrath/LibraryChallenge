c8df1622ded5bd129200613d33443f8d6865c9d7
npatel0620 <npatel0620@gmail.com>
2018-03-30 14:04:41 -0500

    Added book, made changes to program

---
+++ Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryCardCatalog
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string AuthorFirst { get; set; }
+        public string AuthorLast { get; set; }
+        public string Genre { get; set; }
+        public int ISBN { get; set; }
+        public string Fiction { get; set; }
+    }
+}
